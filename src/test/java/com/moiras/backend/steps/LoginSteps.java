b0d8ed3ab3f7014b981060c625c6c58291066bfe
QuispeBrianGabriel <quispebriangabriel@gmail.com>
2026-07-28 03:53:44 -0300

    fix: error de formato

--- src/test/java/com/moiras/backend/steps/LoginSteps.java
+++ src/test/java/com/moiras/backend/steps/LoginSteps.java
@@ -1,23 +1,23 @@
 package com.moiras.backend.steps;
 
+import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
-import io.cucumber.java.es.Cuando;
 
 public class LoginSteps {
 
-    @Dado("que el usuario {string} existe con contraseña {string}")
-    public void que_el_usuario_existe_con_contrasena(String username, String password) {
-        throw new io.cucumber.java.PendingException();
-    }
+  @Dado("que el usuario {string} existe con contraseña {string}")
+  public void que_el_usuario_existe_con_contrasena(String username, String password) {
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @Cuando("ingreso las siguientes credenciales:")
-    public void ingreso_las_siguientes_credenciales(io.cucumber.datatable.DataTable dataTable) {
-        throw new io.cucumber.java.PendingException();
-    }
+  @Cuando("ingreso las siguientes credenciales:")
+  public void ingreso_las_siguientes_credenciales(io.cucumber.datatable.DataTable dataTable) {
+    throw new io.cucumber.java.PendingException();
+  }
 
-    @Entonces("ingreso correctamente al sistema")
-    public void ingreso_correctamente_al_sistema() {
-        throw new io.cucumber.java.PendingException();
-    }
+  @Entonces("ingreso correctamente al sistema")
+  public void ingreso_correctamente_al_sistema() {
+    throw new io.cucumber.java.PendingException();
+  }
 }
